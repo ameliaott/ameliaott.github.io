cca3c2dc76af04c3aa2aaa170e358d02334e51ff
AmeliaOtt <160150967+ameliaott@users.noreply.github.com>
2024-04-26 13:52:32 -0400

    Website

---
+++ contentpages/contact/contact.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Hi <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+We will get back to you for this comment/question: <?php echo $_POST["message"]; ?>
+
+</body>
+</html>
